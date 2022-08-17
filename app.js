e3da59fef2aa019f7ce24dd31175c8eeddfa74e2
Paul-Adrien <pauladrienbourgue@gmail.com>
2022-08-17 20:46:21 +0200

    adding hero header typewriter effect

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const customCursor = document.querySelector('.custom-cursor');
+const title = document.querySelector('h1');
+const subtitle = document.querySelector('.subtitle');
+const heroPushLink = document.querySelector('.hero-push-link');
+const text = 'Porsche, set free.';
 
 window.addEventListener('mousemove', handleCustomCursor);
 
@@ -6,3 +10,22 @@ function handleCustomCursor(e) {
   customCursor.style.transform = `translate(calc(${e.clientX}px - 50%), calc(${e.clientY}px
         - 50%))`;
 }
+
+function typewriter(word, index) {
+  if (index > 3) {
+    subtitle.classList.add('active');
+  }
+  if (index > 6) {
+    heroPushLink.classList.add('active');
+  }
+  if (index < text.length) {
+    setTimeout(() => {
+      title.innerHTML += `<span>${text[index]}</span>`;
+      typewriter(text, index + 1);
+    }, 200);
+  }
+}
+
+setTimeout(() => {
+  typewriter(text, 0);
+}, 300);
